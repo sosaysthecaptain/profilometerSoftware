5eed84532cdb195e70dce97c0005fa5bfa624c90
Marc Auger <sosaysthecaptain@gmail.com>
2018-03-25 12:01:18 -0500

    adding upload functionality

--- src/app/services/camera.service.ts
+++ src/app/services/camera.service.ts
@@ -2,19 +2,58 @@ import { Injectable } from '@angular/core';
 import { NodeWebcam } from 'node-webcam';
 import { UploadService } from './upload.service';
 //import { Command } from '../models/Command';
+import { AngularFireStorage, AngularFireUploadTask } from 'angularfire2/storage';
+import { AngularFirestore } from 'angularfire2/firestore';
+import { Observable } from 'rxjs/Observable';
+import { tap } from 'rxjs/operators';
 
 
 @Injectable()
 export class CameraService {
+  // upload related
+  task: AngularFireUploadTask;
+  percentage: Observable<number>;
+  snapshot: Observable<any>;
+  downloadURL: Observable<string>;
 
-  constructor(public uploadService:UploadService) { }
+  constructor(public uploadService:UploadService,
+              private storage: AngularFireStorage,
+              private db: AngularFirestore) { }
+
+  uploadImage(file: File) {
+    // The storage path
+    const path = `test/${new Date().getTime()}_${file.name}`;
+
+    // Totally optional metadata
+    const customMetadata = { metaString: 'Test metadata' };
+
+    // The main task
+    this.task = this.storage.upload(path, file, { customMetadata })
+
+    // Progress monitoring
+    this.percentage = this.task.percentageChanges();
+    this.snapshot   = this.task.snapshotChanges().pipe(
+      tap(snap => {
+        console.log(snap)
+        if (snap.bytesTransferred === snap.totalBytes) {
+          // Update firestore on completion
+          this.db.collection('photos').add( { path, size: snap.totalBytes })
+        }
+      })
+    )
+    // The file's download URL
+    this.downloadURL = this.task.downloadURL(); 
+  }
 
   cameraParser(data) {
     if(data.body == 'captureImage') {
       console.log('captureImage command received');
       this.uploadService.markAsComplete(data);
-      
     }
+  }
+
+  captureImage() {
+    console.log('captureImage firing');
   }
 
 }
--- src/app/services/gpio.service.ts
+++ src/app/services/gpio.service.ts
@@ -1,13 +1,40 @@
 import { Injectable } from '@angular/core';
 import { gpio } from 'tinker-gpio';
+import { UploadService } from './upload.service';
 
 @Injectable()
 export class GpioService {
 
-  constructor() { }
+  constructor(public uploadService:UploadService) { }
+  // machine state variables
+  xPos = 0;
+  yPos = 0;
+  zPos = 0;
+
 
   gpioParser(data) {
-    console.log('GpioParser receiving command: ' + data.body);
+    if(data.body == 'moveXBy') {
+      console.log('moveXBy command received');
+      this.uploadService.markAsComplete(data);
+    }
   }
+
+  // relative axis movement
+  
+  // absolute axis movement
+  moveXBy() {
+    
+  }
+
+  moveYBy() {
+
+  }
+
+  moveZBy() {
+
+  }
+  // other base GPIO
+
+  
 
 }
